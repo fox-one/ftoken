a8c70a49cb4da7fe25cb8815d894c9778579234d
charlie <ys.wu35@gmail.com>
2022-09-07 08:44:56 +0900

    fix ci license

--- handler/system/info.go
+++ handler/system/info.go
@@ -16,7 +16,7 @@ func HandleInfo(system core.System, factories []core.Factory) http.HandlerFunc {
 				"asset_id": factory.GasAsset(),
 			}
 		}
-		
+
 		render.JSON(w, render.H{
 			"version":   system.Version,
 			"client_id": system.ClientID,
