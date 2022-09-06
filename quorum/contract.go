5f3a5aca923d8e76187cc5fed9be57d6a486b022
charlie <ys.wu35@gmail.com>
2022-09-07 07:54:55 +0900

    tools go.mod; disable solc generate

--- quorum/contract.go
+++ quorum/contract.go
@@ -1,4 +1,4 @@
 package quorum
 
-//go:generate solc --optimize --abi contract/factory.sol -o contract/abi --overwrite
-//go:generate abigen --abi=contract/abi/FTokenFactory.abi --pkg=quorum --out=factory.abi.go
+// //go:generate solc --optimize --abi contract/factory.sol -o contract/abi --overwrite
+// //go:generate abigen --abi=contract/abi/FTokenFactory.abi --pkg=quorum --out=factory.abi.go
